aadbda6fb5e77dfa20fbfc962eea7f45aeafed03
lapassesungs <tahielgiordana@gmail.com>
2021-04-12 13:15:24 -0300

    Agregue la logica de la fpu
    Obtiene las variables desde la pila.
    Aplica las operaciones en orden para formar la formula resolvente.
    
    No almacena las raices obtenidas.
    Falta adaptar la funcion para que acepte variables tipo double.

--- Codigo/formulaResolvente.asm
+++ Codigo/formulaResolvente.asm
@@ -1,3 +1,6 @@
+section .data
+    cuatroNeg dw -4
+
 section .text
 
 global formulaResolvente
@@ -8,10 +11,54 @@ formulaResolvente:
     push ebp
     mov ebp,esp
 
-    mov eax, [ebp+16]
-    mov ebx, [ebp+12]
-    mov ecx, [ebp+8]
-        
+    ;(Por ahora con a,b,c = int)
+    ;a = [ebp+8], b=[ebp+12], c=[ebp+16]
+
+    ;FPU
+    ;Obtengo la primer raiz
+
+    fld1                 ; 1
+    fld dword[ebp+8]     ; a,1
+    fscale               ; 2a,1
+    fdivp st1            ; 1/(2a)
+    
+    fild word[cuatroNeg] ; -4,1/(2a)
+    fld dword[ebp+8]     ; a,-4,1/(2a)
+    fmulp st1            ; (-4a),1/(2a)
+    fld dword[ebp+16]    ; c,(-4a),1/(2a)
+    fmulp st1            ; -4ac,1/(2a)
+    fld dword[ebp+12]    ; b,-4ac,1/(2a)
+    fld dword[ebp+12]    ; b,b,-4ac,1/(2a)
+    fmulp st1            ; b^2,-4ac,1/(2a)
+    faddp st1            ; b^2 - 4ac,1/(2a)
+    fsqrt                ; sqrt(b^2 - 4ac),1/(2a)
+    fld dword[ebp+12]    ; b,sqrt(b^2 - 4ac),1/(2a)
+    fchs                 ; -b,sqrt(b^2 - 4ac),1/(2a)
+    faddp st1            ; -b + sqrt(b^2 - 4ac),1/(2a)
+    fmulp st1            ; (-b + sqrt(b^2 - 4ac)) / 2a
+
+    ;Repito el proceso para la segunda raiz
+    
+    fld1                 ; 1
+    fld dword[ebp+8]     ; a,1
+    fscale               ; 2a,1
+    fdivp st1            ; 1/(2a)
+    
+    fild word[cuatroNeg] ; -4,1/(2a)
+    fld dword[ebp+8]     ; a,-4,1/(2a)
+    fmulp st1            ; (-4a),1/(2a)
+    fld dword[ebp+16]    ; c,(-4a),1/(2a)
+    fmulp st1            ; -4ac,1/(2a)
+    fld dword[ebp+12]    ; b,-4ac,1/(2a)
+    fld dword[ebp+12]    ; b,b,-4ac,1/(2a)
+    fmulp st1            ; b^2,-4ac,1/(2a)
+    faddp st1            ; b^2 - 4ac,1/(2a)
+    fsqrt                ; sqrt(b^2 - 4ac),1/(2a)
+    fld dword[ebp+12]    ; b,sqrt(b^2 - 4ac),1/(2a)
+    fchs                 ; -b,sqrt(b^2 - 4ac),1/(2a)
+    fsubp st1            ; -b - sqrt(b^2 - 4ac),1/(2a)
+    fmulp st1            ; (-b - sqrt(b^2 - 4ac)) / 2a
+    
     jmp end
     
 end:
