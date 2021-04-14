61f49357ce034eef029ad0959fd70ae5cc87df3c
lapassesungs <tahielgiordana@gmail.com>
2021-04-14 16:29:11 -0300

    El usuario puede ingresar los valores de a, b y c

--- Codigo/formulaResolvente.c
+++ Codigo/formulaResolvente.c
@@ -3,22 +3,35 @@
 int formulaResolvente(float a, float b, float c, float *raiz1, float *raiz2);
 
 int main(){
-    float a = 2.0;
-    float b = 0.0;
-    float c = -32.0;
+    float a;
+    float b;
+    float c;
 
     float raiz1;
     float raiz2;
 
     int tieneSolucion;
 
+    printf("\nIngrese el valor de a: ");
+    scanf("%f",&a);
+    printf("\nIngrese el valor de b: ");
+    scanf("%f",&b);
+    printf("\nIngrese el valor de c: ");
+    scanf("%f",&c);
+
     tieneSolucion = formulaResolvente(a,b,c,&raiz1,&raiz2);
 
     if(tieneSolucion == 1){
-        printf("Las raices son: %f y %f\n",raiz1,raiz2);
+        if(raiz1 == raiz2){
+            printf("\nEl conjunto de raices es: {%f}\n",raiz1);
+        }
+        else{
+            printf("\nEl conjunto de raices es: {%f, %f}\n",raiz1,raiz2);
+        }
+              
     }
     else{
-        printf("No existen raices dentro de los numeros reales\n");
+        printf("\nNo existen raices dentro de los numeros reales\n");
     }
     
 }
